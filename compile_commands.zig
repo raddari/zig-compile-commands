1b061dcc7294965e939433ec4a44efb740242d92
raddari <raddari@tuta.io>
2024-10-20 15:50:24 +1030

    upgrade to zig 0.14

--- compile_commands.zig
+++ compile_commands.zig
@@ -214,11 +214,11 @@ fn getCSources(b: *std.Build, steps: []const *std.Build.Step.Compile) []*Absolut
     return res.toOwnedSlice() catch @panic("OOM");
 }
 
-fn makeCdb(step: *std.Build.Step, prog_node: std.Progress.Node) anyerror!void {
+fn makeCdb(step: *std.Build.Step, make_opts: std.Build.Step.MakeOptions) anyerror!void {
     if (compile_steps == null) {
         @panic("No compile steps registered. Programmer error in createStep");
     }
-    _ = prog_node;
+    _ = make_opts;
     const allocator = step.owner.allocator;
     const b = step.owner;
     // NOTE: these are not sane defaults really, but atm I don't care about accurately providing the
